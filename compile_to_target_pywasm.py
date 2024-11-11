1928e2d8bdf09780b1857ee242e7e903e13abb1f
thinkerhui <thinkerhui@qq.com>
2024-11-11 17:07:55 +0800

    refactor：优化编译打印输出，优化代码结构。

--- compile_to_target_pywasm.py
+++ compile_to_target_pywasm.py
@@ -1,15 +1,16 @@
 import os
-
+from common_utils import get_logger
+logger = get_logger()
 BENCHMARK_DIR = os.path.join('..', 'pybenchmarks', 'bencher', 'programs')
 TARGET_DIR = os.path.join('..', 'targets_pywasm')
 
-# BENCHMARKS = [
-#     "binarytrees", "fannkuchredux", "fastaredux", "fibonacci", "jsonbench",
-#     "mandelbrot", "nbody", "pystone", "revcomp", "spectralnorm", "threadring",
-#     "chameneosredux", "fasta", "fib50", "iobench", "knucleotide", "meteor",
-#     "pidigits", "regexdna", "richards", "templates"
-# ]
-BENCHMARKS = ['binarytrees',"knucleotide"]
+BENCHMARKS = [
+    "binarytrees", "fannkuchredux", "fastaredux", "fibonacci", "jsonbench",
+    "mandelbrot", "nbody", "pystone", "revcomp", "spectralnorm", "threadring",
+    "chameneosredux", "fasta", "fib50", "iobench", "knucleotide", "meteor",
+    "pidigits", "regexdna", "richards", "templates"
+]
+# BENCHMARKS = ['binarytrees']
 SOURCE_SUFFIX = '.python3'
 TARGET_SUFFIX = '.wasm'
 COMPILER = 'py2wasm'
@@ -20,18 +21,25 @@ def compile_all():
     遍历BENCHMARKS列表中的每个基准程序，利用py2wasm将其源代码编译成WASM格式，并保存到目标目录中。
     """
     for benchmark in BENCHMARKS:
-        print('Compile '+ benchmark + ' to wasm...')
+        logger.info('Compile '+ benchmark + ' to wasm...')
         out_dir = os.path.join(TARGET_DIR,benchmark)
         os.makedirs(out_dir, exist_ok=True)
         # 找出后缀为.python3的文件(有些benchmark只有一个.python3，一般有多个)
         for file in os.listdir(os.path.join(BENCHMARK_DIR, benchmark)):
             if file.endswith(SOURCE_SUFFIX):
-                command = '%s %s -o %s' % (COMPILER, os.path.join(BENCHMARK_DIR, benchmark, file), os.path.join(out_dir,file)+TARGET_SUFFIX)
-                print(command)
+                target_file = os.path.join(out_dir,file)+TARGET_SUFFIX
+                source_file = os.path.join(BENCHMARK_DIR, benchmark, file)
+                if os.path.exists(target_file):
+                    logger.info('%s already exists, skip.' % target_file)
+                    continue
+                command = '%s %s -o %s' % (COMPILER, source_file, target_file)
+                logger.info(command)
                 os.system(command)
         print()
 def main():
+    logger.info('Compile all benchmarks to wasm...')
     compile_all()
+    logger.info('Done.')
     
 if __name__ == '__main__':
     main()
